434e1bc1facee3084d071c78e598f8496ec130e2
Ahmed Salama <ahmedmsalama9@gmail.com>
2025-12-30 16:54:41 +0200

    d2.1

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,3 +1,8 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 
 export const GET = handleAuth();
+
+console.log('KINDE_ISSUER_URL:', process.env.KINDE_ISSUER_URL);
+if (!process.env.KINDE_ISSUER_URL) {
+  throw new Error("Missing KINDE_ISSUER_URL in environment variables");
+}
