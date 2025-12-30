18238ba29210262af57af7e8d85e19f5c8965a77
Ahmed Salama <ahmedmsalama9@gmail.com>
2025-12-30 17:35:45 +0200

    d2.5

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,19 +1,4 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export const GET = async (req: Request, context: any) => {
-  if (!process.env.KINDE_ISSUER_URL) {
-    throw new Error("Missing KINDE_ISSUER_URL in environment variables");
-  }
-
-  const handler = handleAuth();
-  return handler(req, context);
-};
-
-export const POST = async (req: Request, context: any) => {
-  if (!process.env.KINDE_ISSUER_URL) {
-    throw new Error("Missing KINDE_ISSUER_URL in environment variables");
-  }
-
-  const handler = handleAuth();
-  return handler(req, context);
-};
+export const GET = handleAuth();
+export const POST = handleAuth();
