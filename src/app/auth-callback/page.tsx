5ab372937cdaceb35c9add2485c21f157c366275
Ahmed Salama <ahmedmsalama9@gmail.com>
2025-12-30 15:34:35 +0200

    deployment optimization

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,13 +1,13 @@
-import { useRouter, useSearchParams } from 'next/navigation'
+// import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 
 function page() {
-    const router = useRouter();
-    const searchParams = useSearchParams()
-    const origin = searchParams.get('origin')
+    // const router = useRouter();
+    // const searchParams = useSearchParams()
+    // const origin = searchParams.get('origin')
     return (
         <div>
-            
+            <p>Handeling backend</p>
         </div>
     )
 }
